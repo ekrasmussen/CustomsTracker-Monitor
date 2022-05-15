a1cfe239f331e1bdadaca5f9fc1f377747ac70ad
Emil Rasmussen <emil@erasmussen.dk>
2022-05-15 12:57:11 +0200

    Added Database UpdateManager

---
+++ CustomsTracker-Monitor/Manager/UpdateManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomsTracker_Monitor.Manager
+{
+    internal class UpdateManager
+    {
+        public int Interval { get; set; }
+        // Add private controllers
+
+        public UpdateManager(int interval, CancellationToken token)
+        {
+            Interval = interval;
+            Task.Run(() => StartTimer(token));
+        }
+
+        async Task StartTimer(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await OnTick(token);
+                await Task.Delay(Interval);
+            }
+        }
+
+        async Task OnTick(CancellationToken token)
+        {
+            // Do the Database Stuff here every tick
+        }
+    }
+
+
+}
--- CustomsTracker-Monitor/Program.cs
+++ CustomsTracker-Monitor/Program.cs
@@ -3,10 +3,3 @@ using CustomsTracker_Monitor.Model.APIModel;
 using CustomsTracker_Monitor.Services.APIAccess;
 using System.Threading.Tasks;
 
-MatchAccess access = new MatchAccess();
-AccountAccess accountAccess = new AccountAccess();
-
-
-AccountResponse response = await accountAccess.GetAccountByUsername("Zua", "CHONK");
-
-Console.WriteLine(response.Data.Puuid);
